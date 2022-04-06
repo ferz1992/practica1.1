e001e1d5996569de1050210e944cff6ce72c6574
Fernando Zalazar <f.zalazar@live.com>
2022-04-06 10:18:34 -0300

    agregue algorimos

---
+++ Algoritmos.js
@@ -0,0 +1,7 @@
+console.log("Holaaa¡¡");
+
+document.write("algoo")
+
+document.write("<h1>titulo primero</h1>")
+
+document.write("<img src='https://ichef.bbci.co.uk/news/800/cpsprodpb/15665/production/_107435678_perro1.jpg.webp' alt='perrito agregado por js'>")
